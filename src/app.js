a3cd7e8559be8e11a70c60801edb1560ff08ba63
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2026-02-03 17:30:52 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const formRoutes = require("./routes/form.routes");
 const app = express();
 
 app.use(helmet());
-//app.use(cors());
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 
